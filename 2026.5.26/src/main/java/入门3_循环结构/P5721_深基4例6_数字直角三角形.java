ea89f8c1272ee8f4ed6d90ea62fb7dbe3e86ce69
王林 <3666423708@qq.com>
2026-05-26 13:28:58 +0800

    2026.5.26

---
+++ 2026.5.26/src/main/java/入门3_循环结构/P5721_深基4例6_数字直角三角形.java
@@ -0,0 +1,10 @@
+package 入门3_循环结构;
+
+import java.util.Scanner;
+
+public class P5721_深基4例6_数字直角三角形 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
